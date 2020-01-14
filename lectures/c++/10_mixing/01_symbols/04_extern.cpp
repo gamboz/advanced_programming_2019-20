ae3215350e9ce97596a2388fed83969436bbc2da
Matteo Gamboz <gamboz@medialab.sissa.it>
2020-01-14 15:14:01 +0100

    notes

--- lectures/c++/10_mixing/01_symbols/04_extern.cpp
+++ lectures/c++/10_mixing/01_symbols/04_extern.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+
+// export symbols with the rules of "C"
+// this also means that one cannot have function overloading
+// of these functions
 extern "C"{
   void hello(){
     std::cout << "Hello, World" << std::endl;
